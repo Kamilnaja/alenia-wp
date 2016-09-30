5d6777a3dde4e0c839328c37e9634d9e2baee368
= <kamilnaja@gmail.com>
2016-09-30 19:01:43 +0200

    wip

--- header.php
+++ header.php
@@ -12,38 +12,57 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<title>Alenia theme</title>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
+<link rel="stylesheet" href="screen.css">
+<!-- ładuje pliki z pluginów i inne -->
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'resto' ); ?></a>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="site-title"><a href="#">Allenia</a></h1>
+					<h2 class="subheader">you always want to read it</h2>
+				</div>
+			</div>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'resto' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+			<div class="row">
+				<div class="col-md-12">
+					<div id="navbar" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav navbar-center">
+							<li><a href="#">Home</a></li>
+							<li><a href="../navbar-fixed-top/">About</a></li>
+							<li><a href="portfolio.html">Portfolio</a></li>
+							<li><a href="../navbar-fixed-top/">Events</a></li>
+							<li><a href="contact.html">Contact</a></li>
+							<li><a href="../navbar-fixed-top/">Search</a></li>
+							<li><a href="about-single.html">About Single</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+	</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
+
+
 
 	<div id="content" class="site-content">
