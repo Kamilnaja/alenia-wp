f611951fe12b2e682515323d90f248fd3ad8601d
= <kamilnaja@gmail.com>
2016-10-07 12:25:20 +0200

    start pracy

---
+++ create.php
@@ -0,0 +1,13 @@
+
+mysql> CREATE TABLE aliens (
+	first_name varchar(30),
+	last_name varchar(30),
+	when_it_happened varchar(30),
+	how_long varchar(30),
+	how_many varchar(30),
+	alien_description varchar(30),
+	what_they_did varchar(100),
+	fang_spotted varchar(10),
+	other varchar(100),
+	email varchar(50)
+	);
